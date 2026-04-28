1083cdcf3df148a28c62dcb5c34c5420d57a6d82
Gabriela Pirato <gabrielapirato2318@gmail.com>
2026-04-28 19:22:28 -0300

    Add DTOs for event end seat response

---
+++ Backend/Application/Models/SeatResponse.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Models
+{
+    public class SeatResponse
+    {
+        public Guid Id { get; set; }
+        public string? RowIdentifier { get; set; }
+        public int SeatNumber { get; set; }
+        public string? Status { get; set; }
+    }
+}
