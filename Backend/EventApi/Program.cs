8fa3b53d9206a4b896a591f33a7de4d6caf38a01
Gabriela Pirato <gabrielapirato2318@gmail.com>
2026-04-29 21:36:19 -0300

    Update Program.cs with repositories and handlers

--- Backend/EventApi/Program.cs
+++ Backend/EventApi/Program.cs
@@ -1,6 +1,12 @@
 using Microsoft.Data.SqlClient;
 using Microsoft.EntityFrameworkCore;
 using Infrastructure.Persistence;
+using Application.Interfaces;
+using Application.UseCases.Events.Handlers;
+using Application.UseCases.Sectors.Handlers;
+using Application.UseCases.Seats.Handlers;
+using Application.UseCases.Reservations.Handlers;
+using Infrastructure.Repositories;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -17,6 +23,22 @@ builder.Services.AddDbContext<AppDbContext>(options =>
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// ─── Repositorios ─────────────────────────────────────────────────
+builder.Services.AddScoped<IEventRepository, EventRepository>();
+builder.Services.AddScoped<ISeatRepository, SeatRepository>();
+builder.Services.AddScoped<ISectorRepository, SectorRepository>();
+builder.Services.AddScoped<IReservationRepository, ReservationRepository>();
+builder.Services.AddScoped<IAuditLogRepository, AuditLRepository>();
+
+// ─── Handlers ─────────────────────────────────────────────────────
+builder.Services.AddScoped<CreateEventHandler>();
+builder.Services.AddScoped<GetAllEventsHandler>();
+builder.Services.AddScoped<GetEventByIdHandler>();
+builder.Services.AddScoped<GetSeatsByEventHandler>();
+builder.Services.AddScoped<GetSectorsByEventHandler>();
+builder.Services.AddScoped<CreateReservationHandler>();
+builder.Services.AddScoped<GetReservationByUserHandler>();
 
 var app = builder.Build();
 
