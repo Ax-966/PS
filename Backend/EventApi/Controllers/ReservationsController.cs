64f462ee110ea9da8560922147d76e1e13dda4b4
Gabriela Pirato <gabrielapirato2318@gmail.com>
2026-04-29 21:30:56 -0300

    Add controllers and register services in Program.cs

---
+++ Backend/EventApi/Controllers/ReservationsController.cs
@@ -0,0 +1,25 @@
+﻿using Application.UseCases.Reservations.Commands;
+using Application.UseCases.Reservations.Handlers;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EventApi.Controllers
+{
+    [ApiController]
+    [Route("api/v1/[controller]")]
+    public class ReservationsController : ControllerBase
+    {
+        private readonly CreateReservationHandler _createReservationHandler;
+
+        public ReservationsController(CreateReservationHandler createReservationHandler)
+        {
+            _createReservationHandler = createReservationHandler;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create([FromBody] CreateReservation command)
+        {
+            var reservation = await _createReservationHandler.HandleAsync(command);
+            return Ok(reservation);
+        }
+    }
+}
---
+++ Backend/EventApi/Controllers/SectorsController.cs
@@ -0,0 +1,26 @@
+﻿using Application.UseCases.Sectors.Handlers;
+using Application.UseCases.Sectors.Queries;
+using Microsoft.AspNetCore.Mvc;
+using Application.Interfaces.CQRS;
+
+namespace EventApi.Controllers
+{
+    [ApiController]
+    [Route("api/v1/[controller]")]
+    public class SectorsController : ControllerBase
+    {
+        private readonly GetSectorsByEventHandler _getSectorsByEventHandler;
+
+        public SectorsController(GetSectorsByEventHandler getSectorsByEventHandler)
+        {
+            _getSectorsByEventHandler = getSectorsByEventHandler;
+        }
+
+        [HttpGet("event/{eventId}")]
+        public async Task<IActionResult> GetByEvent(int eventId)
+        {
+            var sectors = await _getSectorsByEventHandler.HandleAsync(new GetSectorsByEvent { EventId = eventId });
+            return Ok(sectors);
+        }
+    }
+}
