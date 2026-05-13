b5aa08f7fdfb34965121934f5c411f2afe3b53c9
Gabriela Pirato <gabrielapirato2318@gmail.com>
2026-05-13 10:31:37 -0300

    Concurrencia

---
+++ Backend/EventApi/Middlewares/ExceptionMiddleware.cs
@@ -0,0 +1,37 @@
+﻿using Application.Exceptions;
+using System.Net;
+using System.Text.Json;
+
+namespace EventApi.Middlewares;
+
+public class ExceptionMiddleware
+{
+    private readonly RequestDelegate _next;
+
+    public ExceptionMiddleware(RequestDelegate next) => _next = next;
+
+    public async Task InvokeAsync(HttpContext context)
+    {
+        try
+        {
+            await _next(context);
+        }
+        catch (Exception ex)
+        {
+            context.Response.ContentType = "application/json";
+
+            // Aquí centralizamos los códigos de error
+            var statusCode = ex switch
+            {
+                ConcurrencyException => HttpStatusCode.Conflict,           // 409
+                SeatReservationConflictException => HttpStatusCode.Conflict, // 409
+                KeyNotFoundException => HttpStatusCode.NotFound,             // 404
+                _ => HttpStatusCode.InternalServerError                      // 500
+            };
+
+            context.Response.StatusCode = (int)statusCode;
+            var result = JsonSerializer.Serialize(new { message = ex.Message });
+            await context.Response.WriteAsync(result);
+        }
+    }
+}
